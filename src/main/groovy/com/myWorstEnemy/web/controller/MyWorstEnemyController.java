cf1f897f00d77bfe2652a1d5af813490a9734de9
Styrfire <g_n_rogers1991@yahoo.com>
2018-01-10 22:01:58 -0600

    Started load champion data endpoint and added some sample json data

--- src/main/groovy/com/myWorstEnemy/web/controller/MyWorstEnemyController.java
+++ src/main/groovy/com/myWorstEnemy/web/controller/MyWorstEnemyController.java
@@ -1,15 +1,18 @@
 package com.myWorstEnemy.web.controller;
 
+import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.riot.api.RiotApi;
 import com.riot.dto.Match.MatchList;
 import com.riot.dto.StaticData.Champion;
+import com.riot.dto.StaticData.ChampionList;
 import com.riot.dto.Summoner.Summoner;
 import com.riot.exception.RiotApiException;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.InputStreamReader;
 import java.util.*;
 
 @RestController
@@ -95,40 +98,6 @@ public class MyWorstEnemyController
 			}
 		} while (swapped);
 
-// static champion data mock
-//		List<Champion> championList = new ArrayList<>();
-//		Champion zac = new Champion();
-//		zac.setTitle("the Secret Weapon");
-//		zac.setName("Zac");
-//		zac.setKey("Zac");
-//		zac.setId(154);
-//		Champion nocturne = new Champion();
-//		nocturne.setTitle("the Eternal Nightmare");
-//		nocturne.setName("Nocturne");
-//		nocturne.setKey("Nocturne");
-//		nocturne.setId(56);
-//		Champion jhin = new Champion();
-//		jhin.setTitle("the Virtuoso");
-//		jhin.setName("Jhin");
-//		jhin.setKey("Jhin");
-//		jhin.setId(202);
-//		Champion sejuani = new Champion();
-//		sejuani.setTitle("Fury of the North");
-//		sejuani.setName("Sejuani");
-//		sejuani.setKey("Sejuani");
-//		sejuani.setId(113);
-//		Champion amumu = new Champion();
-//		amumu.setTitle("the Sad Mummy");
-//		amumu.setName("Amumu");
-//		amumu.setKey("Amumu");
-//		amumu.setId(32);
-//
-//		championList.add(zac);
-//		championList.add(nocturne);
-//		championList.add(jhin);
-//		championList.add(sejuani);
-//		championList.add(amumu);
-
 		int upperBound = 5;
 		if (listOfChampionIds.length < 5)
 			upperBound = listOfChampionIds.length;
@@ -157,10 +126,6 @@ public class MyWorstEnemyController
 		topFiveChampions.add("champions", championJsonArr);
 		System.out.println(topFiveChampions.toString());
 
-//		JsonObject obj = new JsonObject();
-//		obj.addProperty("summonerId", summoner.getId());
-//		System.out.println(obj.toString());
-
 		return topFiveChampions.toString();
 	}
 
@@ -170,5 +135,22 @@ public class MyWorstEnemyController
 		// last 10 matches
 		// grade
 		return "Champion id = " + championId;
+	}
+
+	@RequestMapping(method = RequestMethod.GET, value = "/loadChampionTable")
+	public boolean loadChampionTable()
+	{
+		try
+		{
+			ClassLoader classLoader = getClass().getClassLoader();
+			ChampionList championList = (new Gson()).fromJson(new InputStreamReader(classLoader.getResourceAsStream("json/getStaticChampionData.json")), ChampionList.class);
+			championList.getData().get("154");
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+
+		return true;
 	}
 }
